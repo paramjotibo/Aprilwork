f594b7c778e7fd2699f55a1f8d9e9ec9e799eb66
param singh <param759@gmail.com>
2022-04-30 00:37:38 -0400

    first commit

---
+++ april30/src/test/java/work/april30/firstTest.java
@@ -0,0 +1,18 @@
+package work.april30;
+
+import org.testng.annotations.Test;
+
+public class firstTest {
+
+	@Test
+	public void homework()
+	{
+		System.out.println("work to be done");
+	}
+	
+	@Test
+	public void homework2()
+	{
+		System.out.println("work not to be done");
+	}
+}
